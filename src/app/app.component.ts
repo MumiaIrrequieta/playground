5f3e0154cced5f518b2d35ef88ca6727ea3ed50a
jrodrigues <j.cavaco.rodrigues@gmail.com>
2022-06-17 12:45:37 +0100

    Webcam project test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  private trigger: Subject<void> = new Subject<void>();
+  
   title = 'havacow';
+  webcamImage?: WebcamImage;
+
+  triggerSnapshot(): void {
+   this.trigger.next();
+  }
+  
+  handleImage(webcamImage: WebcamImage): void {
+   console.info('Saved webcam image', webcamImage);
+   this.webcamImage = webcamImage;
+  }
+   
+  get triggerObservable(): Observable<void> {
+   return this.trigger.asObservable();
+  }
 }
